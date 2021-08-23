3f7728a37503c7be919dbdfab020ecbda92024eb
impulse <2535189010@qq.com>
2021-08-24 00:00:30 +0800

    【学习】熟悉GameFramework流程模块

---
+++ Assets/Scripts/Data/GridMap.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMap
+{
+    public int Width = 0;
+    public int Height = 0;
+}
---
+++ Assets/Scripts/Procedure/ProcedureBattle.cs
@@ -0,0 +1,14 @@
+﻿using GameFramework;
+using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class ProcedureBattle : ProcedureBase
+{
+    protected override void OnEnter(ProcedureOwner procedureOwner)
+    {
+        base.OnEnter(procedureOwner);
+
+        Log.Info("进入战斗流程。");
+    }
+}
