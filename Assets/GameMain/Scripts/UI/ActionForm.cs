de668c83ba97d67cbdc96301f058e7a4959fbaf4
impulse <2535189010@qq.com>
2021-10-13 00:22:13 +0800

    【UI】ItemList逻辑

--- Assets/GameMain/Scripts/UI/ActionForm.cs
+++ Assets/GameMain/Scripts/UI/ActionForm.cs
@@ -13,6 +13,8 @@ namespace SSRPG
         private Button m_SkillBtn = null;
         private Button m_AwaitBtn = null;
 
+        private UIListTemplate m_SkillList = null;
+
         private BattleUnitActionState m_ActionState = null;
 
         protected override void OnInit(object userData)
@@ -20,9 +22,15 @@ namespace SSRPG
             base.OnInit(userData);
 
             m_ActionList = transform.Find("m_ActionList").gameObject;
-            m_AttackBtn = transform.Find("m_ActionList/m_AttackBtn").GetComponent<Button>();
-            m_SkillBtn = transform.Find("m_ActionList/m_SkillBtn").GetComponent<Button>();
-            m_AwaitBtn = transform.Find("m_ActionList/m_AwaitBtn").GetComponent<Button>();
+            m_AttackBtn = m_ActionList.transform.Find("m_AttackBtn").GetComponent<Button>();
+            m_SkillBtn = m_ActionList.transform.Find("m_SkillBtn").GetComponent<Button>();
+            m_AwaitBtn = m_ActionList.transform.Find("m_AwaitBtn").GetComponent<Button>();
+            m_SkillList = m_SkillBtn.transform.Find("m_SkillList").GetComponent<UIListTemplate>();
+
+            m_AttackBtn.onClick.AddListener(() => { OnClickBtn(ActionType.Attack); });
+            m_AwaitBtn.onClick.AddListener(() => { OnClickBtn(ActionType.Await); });
+            m_SkillBtn.onClick.AddListener(OnClickSkillBtn);
+            m_SkillList.AddListener(OnShowSkillBtn);
         }
 
         protected override void OnOpen(object userData)
@@ -37,13 +45,45 @@ namespace SSRPG
             }
 
             transform.position = Camera.main.WorldToScreenPoint(m_ActionState.ActiveBattleUnit.transform.position);
-            m_AttackBtn.onClick.AddListener(delegate () { OnClickBtn(ActionType.Attack); });
-            m_AwaitBtn.onClick.AddListener(delegate () { OnClickBtn(ActionType.Await); });
+            m_SkillList.InitList();
+        }
+
+        protected override void OnClose(bool isShutdown, object userData)
+        {
+            base.OnClose(isShutdown, userData);
+
+        }
+
+        private void OnShowSkillBtn(int index)
+        {
+            var item = m_SkillList.GetItem(index);
+            var button = item.GetComponent<Button>();
+            var text = item.GetComponentInChildren<Text>();
+
+            var cfg = GameEntry.Cfg.Tables.TblBattleUnitSkill.Get(index);
+            item.name = string.Format("SkillBtn_{0}", cfg.Id);
+            text.text = cfg.Name;
+            button.onClick.AddListener(() => { OnClickSkillOptionBtn(index); });
         }
 
         private void OnClickBtn(ActionType type)
         {
             m_ActionState.SelectAction(type);
+        }
+
+        private void OnClickSkillBtn()
+        {
+            m_SkillList.RemoveAllItems();
+            var skillIdList = m_ActionState.ActiveBattleUnit.Data.SkillList;
+            for (int i = 0; i < skillIdList.Count; ++i)
+            {
+                m_SkillList.AddItem(skillIdList[i]);
+            }
+        }
+
+        private void OnClickSkillOptionBtn(int skillId)
+        {
+            Log.Info(skillId);
         }
     }
 }
---
+++ Assets/GameMain/Scripts/UI/UIListTemplate.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityGameFramework.Runtime;
+
+namespace SSRPG
+{
+    public class UIListTemplate : MonoBehaviour
+    {
+        [SerializeField]
+        private UIItemTemplate m_ItemTemplate = null;
+
+        private Dictionary<int, UIItemTemplate> m_ItemList = null;
+
+        private UnityAction<int> m_OnShowItem;
+
+        private void Awake()
+        {
+            if (m_ItemTemplate == null)
+            {
+                Log.Warning("UIItemTemplate is null!!!");
+            }
+
+            m_ItemList = new Dictionary<int, UIItemTemplate>();
+        }
+
+        public void InitList()
+        {
+            RemoveAllItems();
+        }
+
+        public void AddItem(int index)
+        {
+            var item = Instantiate(m_ItemTemplate, transform);
+            item.name = string.Format("Item_{0}", index);
+
+            m_ItemList.Add(index, item);
+            m_OnShowItem(index);
+        }
+
+        public UIItemTemplate GetItem(int index)
+        {
+            return m_ItemList[index];
+        }
+
+        public void RemoveItem(int index)
+        {
+            if (m_ItemList.TryGetValue(index, out var item))
+            {
+                Destroy(item.gameObject);
+                m_ItemList.Remove(index);
+            }
+        }
+
+        public void RemoveAllItems()
+        {
+            foreach (var item in m_ItemList.Values)
+            {
+                Destroy(item.gameObject);
+            }
+            m_ItemList.Clear();
+        }
+
+        public void AddListener(UnityAction<int> OnShowItem)
+        {
+            m_OnShowItem += OnShowItem;
+        }
+    }
+}
